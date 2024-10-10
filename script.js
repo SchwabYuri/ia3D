883c4671dd69032e9ea26b9a5af1ec48e6447b66
SchwabYuri <146956851+SchwabYuri@users.noreply.github.com>
2024-10-10 08:37:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,38 +2,44 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('texto-resultado');
 
 const perguntas = [
     {
         enunciado: "Você gosta da ideia de Inteligência Artificial?",
-        alternativas:[{
-            texto:"Sim",
-            afirmativa:"Afirmativa da alternativa1"},
+        alternativas: [
             {
-                texto:"Não",
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
                 afirmativa: "Afirmativa da alternativa 2"
             }
         ]
-
     },
     {
         enunciado: "Você acha que estamos muito dependentes da IA?",
-        alternativas:[{
-            texto:"Sim",
-            afirmativa:"Afirmativa da alternativa1"},
+        alternativas: [
             {
-                texto:"Não",
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
                 afirmativa: "Afirmativa da alternativa 2"
             }
         ]
     },
     {
         enunciado: "Ela pode se tornar um perigo futuro?",
-        alternativas:[{
-            texto:"Sim",
-            afirmativa:"Afirmativa da alternativa1"},
+        alternativas: [
             {
-                texto:"Não",
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
                 afirmativa: "Afirmativa da alternativa 2"
             }
         ]
@@ -42,21 +48,36 @@ const perguntas = [
 
 let posicao = 0;
 let perguntaAtual;
+let respostas = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (posicao >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = " ";
     mostraAlternativas();
 }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativa){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",function(){
-            posicao++;
-            mostraPergunta();
-
-        })
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmativa;
+    respostas += afirmacoes; respostas = resposta + afirmativa
+    posicao++;
+    mostraPergunta();
+}
+function mostraResultado() {
+    caixaPergunta.textContent = " Em 25 anos...";
+    textoResultado.textContent = respostas;
+    caixaAlternativa.textContent = " ";
+
+}
+mostraPergunta();
